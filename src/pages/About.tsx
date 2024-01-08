588fd6aead15f45d5e8cd00fd52499a71e2ff15e
Matt Linder <linder2015@outlook.com>
2024-01-07 23:50:40 -0500

    adding about and some project info

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,17 +4,27 @@ export default function About() {
   return (
     <section className={styles.main} id="about">
       <div className={styles.top}>
+        <h1>About Me</h1>
+        <p>
+          I’m currently a junior at the University of South Carolina, where I
+          study computer science and psychology. I love learning about new
+          technologies and ideas and as a consequence, I spend the majority of
+          my free time reading books, listening to podcasts, and building things
+          (my house is littered with Arduino components and 3d prints).
+        </p>
+      </div>
+      <div className={styles.columns}>
         <div>
           <h3 className={styles.title}>Reading</h3>
           <img className={styles.image} src="/book.jpeg" alt="" />
         </div>
         <div>
-          <h3 className={styles.title}>Building</h3>
-          <img className={styles.image} src="/keyboard.png" alt="" />
+          <h3 className={styles.title}>Listening To</h3>
+          <img className={styles.image} src="/book.jpeg" alt="" />
         </div>
         <div>
-          <h3 className={styles.title}>Learning</h3>
-          <img className={styles.image} src="/book.jpeg" alt="" />
+          <h3 className={styles.title}>Building</h3>
+          <img className={styles.image} src="/keyboard.png" alt="" />
         </div>
       </div>
     </section>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,9 +5,9 @@ export default function Projects() {
     <section className={styles.main} id="projects">
       <h1>Projects</h1>
       <article className={styles.project}>
-        <h2>Word Catching Journal</h2>
         <div className={styles.content}>
           <div className={styles.left}>
+            <h2>Word Catching Journal</h2>
             <img src="" alt="" className={styles.img} />
           </div>
           <div className={styles.right}>
@@ -18,17 +18,44 @@ export default function Projects() {
               <li>SwiftUI</li>
               <li>SQLite</li>
             </ul>
-            <p>What I learned</p>
+            <h4>Dictionary Social Media App</h4>
+            <p>
+              Using Next.js and SwiftUI, I created a small social media app to
+              learn, log, and share unfamiliar words in the hope to help users
+              expand their vocabulary.
+            </p>
+            <div className={styles.links}>
+              <a
+                className={styles.link}
+                href="https://github.com/mlinder10/WordCatchingJournal"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code (web)
+              </a>
+              <a
+                className={styles.link}
+                href="https://github.com/mlinder10/WordCatchingJournalSwift"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code (iOS)
+              </a>
+              <a
+                className={styles.link}
+                href="https://word-catching-journal.vercel.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Site
+              </a>
+            </div>
           </div>
         </div>
       </article>
 
       <article className={styles.project}>
-        <h2>Task Share</h2>
         <div className={styles.content}>
-          <div className={styles.left}>
-            <img src="/projects/taskShareDashboard.png" alt="" className={styles.img} />
-          </div>
           <div className={styles.right}>
             <h3>Technologies Used</h3>
             <ul className={styles.technologies}>
@@ -38,16 +65,42 @@ export default function Projects() {
               <li>Git</li>
               <li>JSON</li>
             </ul>
-            <p>What I learned</p>
+            <h4>
+              Semester-long group project for CSCE 247 (Software Engineering)
+            </h4>
+            <p>
+              Given two fourteen-day sprints, I, along with my four group mates
+              created a task manager application similar to Github Projects.
+            </p>
+            <a
+              className={styles.link}
+              href="https://github.com/mlinder10/ProjectManagerGUI"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Source Code
+            </a>
+          </div>
+          <div className={styles.left}>
+            <h2>Task Share</h2>
+            <img
+              src="/projects/taskShareDashboard.png"
+              alt=""
+              className={styles.img}
+            />
           </div>
         </div>
       </article>
 
       <article className={styles.project}>
-        <h2>Valet Ventures</h2>
         <div className={styles.content}>
           <div className={styles.left}>
-            <img src="/projects/valetVenturesHome.png" alt="" className={styles.img} />
+            <h2>Valet Ventures</h2>
+            <img
+              src="/projects/valetVenturesHome.png"
+              alt=""
+              className={styles.img}
+            />
           </div>
           <div className={styles.right}>
             <h3>Technologies Used</h3>
@@ -56,7 +109,19 @@ export default function Projects() {
               <li>Next.js</li>
               <li>Nodemailer</li>
             </ul>
-            <p>What I learned</p>
+            <h4>Company page for Valet Ventures</h4>
+            <p>
+              I was contracted by Valet Ventures--a small business based in
+              Columbia, SC--to create a landing page for their company.
+            </p>
+            <a
+              className={styles.link}
+              href="https://valetventures.co"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Site
+            </a>
           </div>
         </div>
       </article>
