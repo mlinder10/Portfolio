de30371ab0f1c27b02f28b74e67b30678d3b069f
Matt Linder <linder2015@outlook.com>
2024-01-17 20:15:59 -0500

    removing carousel

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,114 +1,34 @@
-import { useState } from "react";
-import { VscArrowLeft, VscArrowRight } from "react-icons/vsc";
+import Reveal from "../components/Reveal";
 import styles from "../styles/about.module.css";
 
-type Interests = 0 | 1 | 2;
+const options = { delay: 0.3, duration: 0.8 };
 
 export default function About() {
-  const [show, setShow] = useState<Interests>(0);
-  const offset = 250;
-
-  function left() {
-    if (show === 0) return setShow(2);
-    setShow((show - 1) as Interests);
-  }
-
-  function right() {
-    if (show === 2) return setShow(0);
-    setShow((show + 1) as Interests);
-  }
-
-  function computeStyle(interest: Interests) {
-    const left = {
-      translate: `-${offset}px`,
-      opacity: "0",
-      scale: "0.6",
-      pointerEvents: "none",
-    };
-    const target = { translate: "0", opacity: "1", scale: "1" };
-    const right = {
-      translate: `${offset}px`,
-      opacity: "0",
-      scale: "0.6",
-      pointerEvents: "none",
-    };
-
-    switch (interest) {
-      case 0:
-        switch (show) {
-          case 0:
-            return target;
-          case 1:
-            return left;
-          case 2:
-            return right;
-        }
-      case 1:
-        switch (show) {
-          case 0:
-            return right;
-          case 1:
-            return target;
-          case 2:
-            return left;
-        }
-      case 2:
-        switch (show) {
-          case 0:
-            return left;
-          case 1:
-            return right;
-          case 2:
-            return target;
-        }
-    }
-  }
-
   return (
     <section className={styles.main} id="about">
-      <div className={styles.left}>
-        <h1 className={styles.title}>About Me</h1>
-        <p>
-          {`
+      <Reveal from="left" {...options}>
+        <img src="/book.jpeg" alt="" />
+      </Reveal>
+      <Reveal from="top" {...options}>
+        <div className={styles["text-container"]}>
+          <h1 className={styles.title}>About Me</h1>
+          <p className={styles.text}>
+            {`
             I'm currently a junior at the University of South Carolina, where I
             study computer science and psychology. I love learning about new
             technologies and ideas. As a consequence, I spend the majority of my
             free time reading books, listening to podcasts, and building things.
             Here are some of the places that my interest lies at the moment.
-          `}
-        </p>
-      </div>
-      <div className={styles.right}>
-        <button onClick={left}>
-          <VscArrowLeft />
-        </button>
-        <div className={styles["interest-container"]}>
-          <div className={styles.interest} style={computeStyle(0)}>
-            <img className={styles.img} src="/book.jpeg" alt="" />
-            <div className={styles["txt-box"]}>
-              <h2>Reading</h2>
-              <p>Some Text</p>
-            </div>
-          </div>
-          <div className={styles.interest} style={computeStyle(1)}>
-            <img className={styles.img} src="/podcast.png" alt="" />
-            <div className={styles["txt-box"]}>
-              <h2>Listening</h2>
-              <p>Some Text</p>
-            </div>
-          </div>
-          <div className={styles.interest} style={computeStyle(2)}>
-            <img className={styles.img} src="/keyboard.png" alt="" />
-            <div className={styles["txt-box"]}>
-              <h2>Building</h2>
-              <p>Some Text</p>
-            </div>
-          </div>
+            `}
+          </p>
         </div>
-        <button onClick={right}>
-          <VscArrowRight />
-        </button>
-      </div>
+      </Reveal>
+      <Reveal from="bottom" {...options}>
+        <img src="/podcast.png" alt="" />
+      </Reveal>
+      <Reveal from="right" {...options}>
+        <img src="/keyboard.png" alt="" />
+      </Reveal>
     </section>
   );
 }
