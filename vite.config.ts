9ef6bb0bb93357183b78b36f0d99bcac86422c3f
Matt Linder <linder2015@outlook.com>
2024-12-15 13:24:02 -0500

    changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: "terser", // Switch from esbuild to terser if esbuild causes issues
+    terserOptions: {
+      compress: {
+        drop_console: false, // Prevent console logs from being stripped (if needed)
+        ecma: 2015, // Use appropriate ECMAScript target
+      },
+      output: {
+        comments: true, // Keep comments in the output for debugging
+      },
+    },
+  },
+});
